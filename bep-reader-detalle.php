42baf8e6e5a264c48eb32f59ba5214b170e1eb9b
Rubén Ricarte <ruben.ricarte@gmail.com>
2025-10-30 15:07:31 -0300

    update carga de archivos

--- bep-reader-detalle.php
+++ bep-reader-detalle.php
@@ -3,7 +3,7 @@
 	date_default_timezone_set("Chile/Continental");
 	$time = date('His');
 
-	error_reporting(0);
+	//error_reporting(0);
 
 	if (!defined('INCLUDE_PATH')) {
 	  define('INCLUDE_PATH', 'xrqEi2rpfA73XH9cruLMxt2oZ/');
@@ -12,58 +12,10 @@
 	include_once(INCLUDE_PATH . 'class/class.inputfilter.php');
 	include_once(INCLUDE_PATH . 'class/inc.globals.php');
 	include_once(INCLUDE_PATH . 'class/class.csv.php');
-	include_once(INCLUDE_PATH . 'class/class.registro.php');
 	include_once(INCLUDE_PATH . 'class/class.detalle.php');
+
 	include_once(INCLUDE_PATH . 'class/class.resumen.php');
 	
-	$resumen['resu_inicio'] = date('Y-m-d H:i:s');
-	
-	$objRegistro 	= new registro();
-	$objResumen 	= new resumen();
-	$table 			= "bep";
-
-	$date = date('Ymd');
-	$file_csv 	= 'BEP.csv';
-
-	
-	if(file_exists($file_csv)){
-		$csv 		= new CsvImporter($file_csv,';', true);
-		$datas 		= $csv->get();
-		$count = 0;
-
-		if(is_array($datas) && !empty($datas)){
-			
-			$objRegistro->deleteRegistroByTable($table);
-
-
-			foreach ($datas as $data) { 
-
-				$post['tipo'] 						= $data['TIPO'] ?? "";
-				$post['id'] 						= $data['ID'] ?? "";
-				$post['nombre'] 					= $data['NOMBRE'] ?? "";
-				$post['canal'] 						= $data['CANAL'] ?? "";
-
-				$post['CUMPLIMIENTO_BEP'] 			= $data['CUMPLIMIENTO_BEP'] ?? "";
-				$post['CUMPLIMIENTO_BEP_VOL'] 		= $data['CUMPLIMIENTO_BEP_VOL'] ?? "";
-
-				if( !empty($post['id']) ){
-					$objRegistro->saveRegistroByTable($table, $post);
-					$count++;
-				}
-			}	
-		}
-		$resumen['resu_registros'] 	= $count;
-		$resumen['resu_termino'] 	= date('Y-m-d H:i:s');
-		$resumen['resu_archivo'] 	= $file_csv;
-		$objResumen->saveResumen($resumen);
-		
-	}else{
-		echo "No existe archivo: ".$file_csv;
-	}
-
-
-
-
 
 	$resumen['resu_inicio'] = date('Y-m-d H:i:s');
 	
@@ -72,8 +24,10 @@
 	$table 			= "bep";
 
 	$date = date('Ymd');
+	//$file_csv 	= 'MINIONS_ENERGIA_DETALLE_'.$date.'.csv';
 	$file_csv 	= 'BEP_DETALLE.csv';
 
+	
 	if(file_exists($file_csv)){
 		$csv 		= new CsvImporter($file_csv,';', true);
 		$datas 		= $csv->get();
@@ -108,6 +62,5 @@
 	}else{
 		echo "No existe archivo: ".$file_csv;
 	}
-
 	
 ?>
